8013f91d2c4695019c56560a577afa465a7f9d2a
Sean Shillo <revshillo@yahoo.com>
2013-04-27 15:26:47 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthystuff::Application.config.secret_token = '543ac3f1456ea69854bc1d49933f995201e9973ac9c2e64abfa797e06a6ff5edc9053109defad287a47cd8d583cb0f67e54206289ddaa9a8b08a040bb3beac86'
