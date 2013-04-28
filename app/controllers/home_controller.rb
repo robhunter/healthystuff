d456d506abf4f2a0fdab008ca53d835e21760770
Sean Shillo <revshillo@yahoo.com>
2013-04-27 21:11:53 -0400

    some styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   
   def index
-    
+    @image = "http://crunchbase.com/#{Company.first.image["available_sizes"].last.last}"
+    @overview = Company.first.overview 
   end
   
 end
