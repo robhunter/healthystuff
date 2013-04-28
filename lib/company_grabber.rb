e3efb65c2fdcf663ed8d623362a5542abb66d859
Rob Hunter <rob.v.hunter@gmail.com>
2013-04-28 09:12:58 -0400

    fixed bootstrap

--- lib/company_grabber.rb
+++ lib/company_grabber.rb
@@ -23,4 +23,24 @@ class CompanyGrabber
     end
   end
   
+  def self.get_adaptly
+    a = Company.create(name:"Adapt-ly", category_code:nil, permalink:"adaptly")
+    a.populate_info rescue "#{company.name} has a stupid name"
+    a.name = "Adaptly"
+    g = Company.create(name:"Genes and Us", category_code:nil, permalink:"genes-and-us")
+    g.overview = "<p>Genes and Us helps families understand how their genetics affect their health.<br><br>Couples can connect their 23andMe accounts to understand what traits they'll pass on to their children - their good genes and their bad genes, the traits that they'll definitely pass on and the traits that they may or may not pass on.</p>"
+    g.image = {"available_sizes"=>[[[150, 35], "assets/images/resized/0000/2812/2812v1-max-150x150.png"], [[200, 47], "assets/images/resized/0000/2812/2812v1-max-250x250.png"], [[200, 47], "http://www.genesand.us/assets/square_logo.png"]], "attribution"=>nil} 
+    g.write_attribute('tc_homepage_url', "http://www.genesand.us")
+    g.write_attribute('tc_crunchbase_url', 'genes-and-us')
+    
+    g.save
+    a.save
+    87.times do |t|
+      g.vote('up')
+    end
+    72.times do |t|
+      a.vote('up')
+    end
+  end
+  
 end
