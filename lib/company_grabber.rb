11db475f1d341d09b1f01c5d6aa82f45a10675d0
Rob Hunter <rob.v.hunter@gmail.com>
2013-04-27 20:49:53 -0400

    maybe fucking up the time stream

--- lib/company_grabber.rb
+++ lib/company_grabber.rb
@@ -5,7 +5,19 @@ class CompanyGrabber
   end
   
   def get_entities(page)
-    
+    response = HTTParty.get("http://api.crunchbase.com/v/1/companies.js?page=#{page}&api_key=#{Rails.configuration.crunchbase_key}")
+    puts response.shift
+    response.each do |company|
+      c = Company.create(name:company['name'], category_code:company["category_code"], permalink:company["permalink"])
+    end
+  end
+  
+  def get_last_entries(pages)
+    page = 123
+    pages.times do |i|
+      get_entities(page)
+      page -= 1
+    end
   end
   
 end
